a2aa174b465bc24f7cc49b3c999f72b0ea4cefb0
vicente802 <69038580+vicente802@users.noreply.github.com>
2023-04-19 11:50:37 +0800

    Delete Grading system directory

--- Grading system/javascript/javascript_connection/javascript_cdn.php
+++
@@ -1,4 +0,0 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
-<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
